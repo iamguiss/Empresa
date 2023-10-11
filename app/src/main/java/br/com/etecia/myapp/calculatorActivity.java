2a897cce25241a65e0efad515076c62fb03585b0
iamguiss <guizinhosilvasantos10@gmail.com>
2023-10-10 21:39:47 -0300

    class

--- app/src/main/java/br/com/etecia/myapp/calculatorActivity.java
+++ app/src/main/java/br/com/etecia/myapp/calculatorActivity.java
@@ -6,7 +6,40 @@ import android.os.Bundle;
 
 public class calculatorActivity extends AppCompatActivity {
 
+    public class Calcular{
+        private double txtValorEnter;
+        private double valorporcentagem;
 
+        public Calcular (double txtValorEnter, double valorporcentagem){
+            this.txtValorEnter=txtValorEnter;
+            this.valorporcentagem=valorporcentagem;
+        }
+
+        public double calcularTipo() {
+            return(txtValorEnter * valorporcentagem / 100);
+        }
+
+        public double calcularTotal(){
+            return txtValorEnter + calcularTipo();
+        }
+
+        public double getTxtValorEnter(){
+            return txtValorEnter;
+        }
+        public void setTxtValorEnter(double txtValorEnter){
+            this.txtValorEnter = txtValorEnter;
+        }
+
+        public double getValorporcentagem(){
+            return valorporcentagem;
+        }
+
+        public void setValorporcentagem(double valorporcentagem){
+            this.valorporcentagem = valorporcentagem;
+        }
+    }
+
+    public class
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
