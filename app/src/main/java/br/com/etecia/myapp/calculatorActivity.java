165966896a8041bccafeb0c84050341a8c1d757f
iamguiss <guizinhosilvasantos10@gmail.com>
2023-10-10 21:21:48 -0300

    Gorjeta

--- app/src/main/java/br/com/etecia/myapp/calculatorActivity.java
+++ app/src/main/java/br/com/etecia/myapp/calculatorActivity.java
@@ -1,16 +1,16 @@
 package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class GORJETAActivity extends AppCompatActivity {
+public class calculatorActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.gorjetaactivity);
-
-        Spinner spinner = (Spinner) findViewById(R.id.planets_spinner);
+        setContentView(R.layout.calculator_activity);
     }
-
 }
